f5925f6c244a76f889128d2d13aa3e3014c8f37a
Binyan Li <binyanli@microsoft.com>
2022-09-14 11:05:15 -0700

    Check point, 2022-09-14T11:05:15.

--- gRPC.Web.Server/Program.cs
+++ gRPC.Web.Server/Program.cs
@@ -1,7 +1,14 @@
 using gRPC.Web.Server.Protos;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Setup a HTTP/2 endpoint without TLS.
+    options.ListenLocalhost(7264, o => o.Protocols = HttpProtocols.Http2);
+});
 
 var app = builder.Build();
 app.UseGrpcWeb();
