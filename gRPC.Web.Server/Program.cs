2062326483ddae0e2344e57c0ee7d14fa7aeaece
Binyan Li <binyanli@microsoft.com>
2022-09-14 11:03:54 -0700

    Check point, 2022-09-14T11:03:54.

--- gRPC.Web.Server/Program.cs
+++ gRPC.Web.Server/Program.cs
@@ -1,6 +1,13 @@
+using gRPC.Web.Server.Protos;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddGrpc();
+
 var app = builder.Build();
+app.UseGrpcWeb();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGrpcService<HelloWorldImplService>().EnableGrpcWeb();
 
 app.Run();
