96fa557d06bbd9658eaf14ed60bac06a35050086
Binyan Li <binyanli@microsoft.com>
2022-09-14 10:52:59 -0700

    Set up helloworld.

---
+++ gRPC.Web.Server/Protos/HelloWorldImplService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+namespace gRPC.Web.Server.Protos
+{
+    public class HelloWorldImplService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            var res = new HelloReply
+            {
+                Message = "ctrl + S"
+            };
+            return Task.FromResult(res);
+        }
+    }
+}
